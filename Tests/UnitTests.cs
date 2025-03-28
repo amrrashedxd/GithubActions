579db62dbb1c2a19126fb888c4c167c585afb842
amrrashedxd <73825014+amrrashedxd@users.noreply.github.com>
2025-03-28 13:24:17 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -27,3 +27,85 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
 }
+
+
+[TestClass]
+public class SubtractionTests
+{
+    [TestMethod]
+    public void Subtract_Valid_Elsayed()
+    {
+        Assert.AreEqual(1, Program.Subtract("3", "2"));
+        Assert.AreEqual(-1, Program.Subtract("2", "3"));
+        Assert.AreEqual(5, Program.Subtract("10", "5"));
+    }
+
+    [TestMethod]
+    public void Subtract_Invalid_Elsayed()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+    }
+
+    [TestMethod]
+    public void Subtract_Null_Elsayed()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
+    }
+}
+
+[TestClass]
+public class MultiplicationTests
+{
+    [TestMethod]
+    public void Multiply_Valid_Elsayed()
+    {
+        Assert.AreEqual(6, Program.Multiply("2", "3"));
+        Assert.AreEqual(0, Program.Multiply("0", "5"));
+        Assert.AreEqual(20, Program.Multiply("4", "5"));
+    }
+
+    [TestMethod]
+    public void Multiply_Invalid_Elsayed()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
+    }
+
+    [TestMethod]
+    public void Multiply_Null_Elsayed()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
+    }
+}
+
+[TestClass]
+public class PowerTests
+{
+    [TestMethod]
+    public void Power_Valid_Elsayed()
+    {
+        Assert.AreEqual(8, Program.Power("2", "3"));
+        Assert.AreEqual(1, Program.Power("5", "0"));
+        Assert.AreEqual(9, Program.Power("3", "2"));
+    }
+
+    [TestMethod]
+    public void Power_Invalid_Elsayed()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
+        Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
+    }
+
+    [TestMethod]
+    public void Power_Null_Elsayed()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
+    }
+}
